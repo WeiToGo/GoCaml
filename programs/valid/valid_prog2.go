94aed3ef20fa0482f17b8657123456877c66f380
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2015-02-16 23:43:36 -0500

    fizzbuzz

---
+++ programs/valid/valid_prog2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    for i := 0; i < 100; i++ {
+    	if i % 3 == 0 && i % 5 == 0 {
+    		fmt.Println("fizzbuzz")
+    	} else if i % 3 == 0 {
+    		fmt.Println("fizz");
+    	} else if i % 5 == 0 {
+    		fmt.Println("buzz");
+    	};
+    }
+}
