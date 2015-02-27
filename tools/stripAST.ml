169860c9593dcf5e71ed9a76032ad9abc9cd1870
omarglez <omarglez233@gmail.com>
2015-02-27 04:00:37 -0500

    fixed bug with comments inside brackets, now prints to file

--- tools/stripAST.ml
+++ tools/stripAST.ml
@@ -1,13 +1,21 @@
 (*Strips ast info in between braces{} run with ocaml stripAST.ml file  *)
 
 open Printf
+open String
 
 let input = open_in Sys.argv.(1)
 
 let charStream = Stream.from (fun _ -> try Some(input_char input) with End_of_file -> None )
 
+let outFile =
+   let arg = Sys.argv.(1) in
+   let pos = index arg '.' in
+   let pre = sub arg 0 pos in
+   let name = concat "." [pre; "stripped.mly"] in
+   open_out name
+
 let rec printBody ch =
-   print_char ch;
+   fprintf outFile "%c" ch;
    flush stdout;
    match ch with
    | '{' -> skipChars (Stream.next charStream)
@@ -17,24 +25,38 @@ let rec printBody ch =
    | _ -> printBody (Stream.next charStream)
 and skipChars ch =
    match ch with
-   | '}' -> print_char ch; printBody (Stream.next charStream)
+   | '}' -> fprintf outFile "%c" ch; printBody (Stream.next charStream)
    | '(' -> (match Stream.peek charStream with
-            | Some('*') -> skipComment (Stream.next charStream)
+            | Some('*') -> fprintf outFile "%c" '(';
+                           flush stdout;
+                           skipInComment (Stream.next charStream)
             | _         -> skipChars (Stream.next charStream))
    | _ -> skipChars (Stream.next charStream)
 and skipComment ch =
    match ch with
-   | '*' -> print_char ch;
+   | '*' -> fprintf outFile "%c" ch;
             flush stdout;
             (match Stream.peek charStream with
             | Some(')') -> printBody (Stream.next charStream)
             | _         -> skipComment (Stream.next charStream))
-   | _   -> print_char ch;
+   | _   -> fprintf outFile "%c" ch;
             flush stdout;
             skipComment (Stream.next charStream)
+and skipInComment ch =
+   match ch with
+   | '*' -> fprintf outFile "%c" ch;
+            flush stdout;
+            (match Stream.peek charStream with
+            | Some(')') -> fprintf outFile "%c" ')';
+                           flush stdout;
+                           skipChars (Stream.next charStream)
+            | _         -> skipInComment (Stream.next charStream))
+   | _   -> fprintf outFile "%c" ch;
+            flush stdout;
+            skipInComment (Stream.next charStream)
 
 let rec printAll ch =
-   print_char ch;
+   fprintf outFile "%c" ch;
    flush stdout;
    match ch with
    | '%' -> (match Stream.peek charStream with
@@ -47,4 +69,5 @@ let () =
    try
       printAll (Stream.next charStream)
    with e ->
-      close_in input
+      close_in input;
+      close_out outFile
