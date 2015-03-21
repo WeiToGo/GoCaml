0586f28e5f00066915e5201ad434f308eb6455cc
Wei Gao <weitogo2013@gmail.com>
2015-03-21 10:57:01 -0400

    remove deleted files

--- TA_test_invalid.sh
+++ TA_test_invalid.sh
@@ -2,7 +2,7 @@
 a=$(find dev/TA_test/test_programs/typing_invalid -type f)
 while read -r line; do
    echo "Running test on $line "
-   ./run.sh -pptype -dumpsymtab $line
+   ./run.sh $line
    if [[ $? != 0 ]]; then
        echo "Failed at $line"
        echo "$1"
