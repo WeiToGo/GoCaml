e1b7e9be18b31ca3fd579c1c7ed3781bdad00693
Wei Gao <weitogo2013@gmail.com>
2015-04-01 00:20:15 -0400

    more code print, not compiling

--- src/exp_gen.ml
+++ src/exp_gen.ml
@@ -3,8 +3,8 @@ open Ast
 let lc = ref 0
 
 let print_ast prog pretty = 
-(* 	let Program(pack,ldl) = prog in *)
-	let Program(exp) = prog in
+	let Program(pack,ldl) = prog in
+	(* let Program(exp) = prog in *)
 	let outfile = open_out pretty in 
 	let print_string = fun s -> output_string outfile s in
 	let print_char = fun c -> output_char outfile c in
@@ -13,40 +13,40 @@ let print_ast prog pretty =
 			let print_binop_int op = match op with
 				| BinEq -> 
 					print_string 
-					"if_icmpeq Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1\n";
+					"if_icmpeq Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1\n";
 					 lc := !lc + 1
 				| BinNotEq -> print_string 
-					"if_icmpne Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1";
+					"if_icmpne Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1";
 					 lc := !lc + 1;
 				| BinLess -> print_string 
-					"if_icmplt Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1\n";
+					"if_icmplt Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1\n";
 					 lc := !lc + 1;
 				| BinLessEq -> print_string
-					"if_icmple Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1\n"
+					"if_icmple Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1\n"
 					 lc := !lc + 1;
 				| BinGreater -> print_string
-					"if_icmpgt Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1\n"
+					"if_icmpgt Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1\n"
 					 lc := !lc + 1;
 				| BinGreaterEq -> print_string 
-					"if_icmpge Label_" ^ lc
-					 "iconst_0"
-					 "Label_" ^ lc ^ ":"
-					 "iconst_1\n"
+					"if_icmpge Label_" ^ lc ^ "\n" ^
+					 "iconst_0
+					 Label_" ^ lc ^ ":
+					 iconst_1\n"
 					 lc := !lc + 1;
 				| BinPlus -> print_string "iadd"
 				| BinMinus -> print_string "isub"
@@ -182,40 +182,32 @@ let print_ast prog pretty =
 	in
 	let rec print_multi_struct_field level msf= match msf with
 		| MultipleStructFieldDecl (ssf_list) -> ()
-	and print_type_spec level ts = match ts with
+	and print_type_spec ts = match ts with
 		| BasicType (bt) -> ()
 		| SliceType (t) -> ()
 		| ArrayType (int_lit, t) -> ()
 		| StructType (st) -> ()
-		| FunctionType (tl, ts_op) ->
-			begin
-				print_string " func ( ";
-				List.iter (fun x -> print_type_spec level x) tl;
-				print_string ") ";
-				match ts_op with
-				| None -> ()
-				| Some ts_op ->  print_type_spec level ts_op
-			end
+		| FunctionType (tl, ts_op) -> ()
 		| CustomType (id) -> ()
 	in
-	let print_func_arg level fa = match fa with
+	let print_func_arg fa = match fa with
 		| FunctionArg (id,t) ->
 			begin
 				print_identifier id;
 				print_type_spec level t;
 			end
 	in
-	let print_func_sign level fs = match fs with
+	let print_func_sign fs = match fs with
 		| FunctionSig (f_list, t_op) -> ()
 	in
-	let print_multi_var_decl level mvd = match mvd with
+	let print_multi_var_decl mvd = match mvd with
 	| MultipleVarDecl (svd_list) -> ()
 	in
-	let print_var_decl level mvd_list = ()
+	let print_var_decl mvd_list = ()
 	in
-	let print_short_var_decl level svd_list  = ()
+	let print_short_var_decl svd_list  = ()
 	in
-	let rec print_stmt level stmt = 
+	let rec print_stmt stmt = 
 		match stmt with
 		| EmptyStatement -> ()
 		| ExpressionStatement (e) -> ()
@@ -233,31 +225,20 @@ let print_ast prog pretty =
 		| BreakStatement -> ()
 		| ContinueStatement  -> ()
 		| BlockStatement (sl) -> ()
-	and print_stmt_wrap level stmt = match stmt with
-		| LinedStatement (ln,s) -> print_stmt level s
-	and print_statement_list level st_list = match st_list with
+	and print_stmt_wrap stmt = match stmt with
+		| LinedStatement (ln,s) -> print_stmt s
+	and print_statement_list st_list = match st_list with
 				| h :: t -> 
 					begin
-						print_stmt_wrap (level + 1) h;
- 						print_string ";\n";
- 						print_statement_list level t;
+						print_stmt_wrap h;
+ 						print_statement_list t;
  					end
 				| [] -> ()
 	in
-	let print_top_decl level td = match td with 
-		| FunctionDecl (id,fs, stmt_list) ->
-			begin
-				print_string "func ";
-				print_identifier id;
-				print_func_sign level fs;
-				print_string " { \n";
-				insert_tab(level);
-				print_statement_list level stmt_list;
-				insert_tab(level);
-				print_string "}";
-			end
-		| TypeDeclBlock (tl) -> print_type_decl level tl
-		| VarDeclBlock (vl) -> print_var_decl level vl
+	let print_top_decl td = match td with 
+		| FunctionDecl (id,fs, stmt_list) -> ()
+		| TypeDeclBlock (tl) -> ()
+		| VarDeclBlock (vl) -> ()
 	in
 	let print_lined_top_decl_list level ldl = 
 		List.iter 
@@ -266,5 +247,6 @@ let print_ast prog pretty =
 				let () = print_top_decl level td in
 				print_string ";\n") ldl
 	in
-	print_expr exp;
+	print_lined_top_decl_list ldl;
 	close_out outfile
+
