0ffddb4e9fc4192e9e91ed6b07e8016c0e88f2a5
Wei Gao <weitogo2013@gmail.com>
2015-04-06 18:34:41 -0400

    added more jasmin instructions

--- src/jasminAst.ml
+++ src/jasminAst.ml
@@ -40,7 +40,7 @@ and jinstruction =
   | Iconst_0 | Iconst_1 | Iconst_2 | Iconts_3
   | Ldc of string
   | Dup
-  | BiPush of string 
+  | BiPush of string  (* -128 to 127*)
   | GetStatic of string * jtype
   | PutStatic of string * jtype
   | InvokeVirtual of jmethod_sig
@@ -49,6 +49,19 @@ and jinstruction =
   | Dload of int
   | IStore of int
   | DStore of int
+  | ICmpeq of string
+  | ICmpne of string
+  | ICmplt of string
+  | ICmpgt of string
+  | ICmple of string
+  | ICmpge of string
+  | DCmpg
+  | Ifeq of string
+  | Ifne of string
+  | Iadd | Isub | Imul | Idiv | Irem | Ior | Ixor
+  | Ishl | Ishr | Iand | Ineg 
+  | Dadd | Dsub | Dmul | Ddiv | Drem | Dneg
+
   (* Keep adding more and more instructions here.
    * Then also change the string_of_jinst function below *)
 and pseudo_instruction = 
@@ -73,12 +86,12 @@ let quote_string s = "\"" ^ s ^ "\""
 let rec string_of_jtype = function
 | JVoid -> "V"
 | JByte -> raise NotImplemented
-| JChar -> raise NotImplemented
+| JChar -> "C"
 | JShort -> raise NotImplemented
 | JInt -> "I"
-| JLong -> raise NotImplemented
+| JLong -> "J"
 | JFloat -> "F"
-| JDouble -> raise NotImplemented
+| JDouble -> "D"
 | JRef(s) -> "L" ^ s ^ ";"
 | JArray(atype) -> "[" ^ (string_of_jtype atype)
 | JStruct(s) -> raise NotImplemented
@@ -106,6 +119,33 @@ let string_of_jinst = function
 | Dload(i) -> "dload " ^ (string_of_int i)
 | IStore(i) -> "istore " ^ (string_of_int i)
 | DStore(i) -> "dstore " ^ (string_of_int i) 
+| ICmpeq(l) -> "if_icmpeq" ^ l
+| ICmpne(l) -> "if_icmpne" ^ l
+| ICmplt(l) -> "if_icmplt" ^ l
+| ICmpgt(l) -> "if_icmpgt" ^ l
+| ICmple(l) -> "if_icmple" ^ l
+| ICmpge(l) -> "if_icmpge" ^ l
+| DCmpg -> "dcmpg"
+| Ifeq(l) -> "ifeq" ^ l  
+| Ifne(l) -> "ifne" ^ l
+| Iadd -> "iadd"
+| Isub -> "isub"
+| Imul -> "imul"
+| Idiv -> "idiv"
+| Irem -> "irem"
+| Ior -> "ior"
+| Ixor -> "ixor"
+| Ishl -> "ishl"
+| Ishr -> "ishr"
+| Iand -> "iand"
+| Ineg -> "ineg"
+| Dadd -> "dadd"
+| Dsub -> "dsub"
+| Dmul -> "dmul"
+| Ddiv -> "ddiv"
+| Drem -> "drem"
+| Dneg -> "dneg"
+
 
 let calculate_local_limit jstmts = 25  (* Not implemented yet *)
 let calculate_ostack_limit jstmts = 25 (* Not implemented yet *) 
@@ -122,6 +162,7 @@ let get_global_var_map gvar_entry_list =
   List.fold_left
     (fun cur_map new_entry -> 
       let { var_number = n; _ } = new_entry in
+      
       GlobalVarMap.add n new_entry cur_map )
     GlobalVarMap.empty
     gvar_entry_list
