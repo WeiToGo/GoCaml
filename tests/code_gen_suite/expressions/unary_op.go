eaebfbf934b72ee8cd2424a7bcff4685a2b9029d
Wei Gao <weitogo2013@gmail.com>
2015-04-07 15:22:28 -0400

    added unary op test files

---
+++ tests/code_gen_suite/expressions/unary_op.go
@@ -0,0 +1,18 @@
+package main 
+
+func main () {
+
+// int
+	println(+(34*5/3))  
+	println(-(34-63)) 
+	println(-(-234*3)) 
+	println(^56)
+	println(^(34+ (-3))) 
+
+//rune
+
+
+//bool
+	println(!(4>5))
+	println(!((3>4) || (4!=5)))
+}
