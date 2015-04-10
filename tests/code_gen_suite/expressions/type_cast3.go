b968723585904c1902158078faa0777028367f9f
Wei Gao <weitogo2013@gmail.com>
2015-04-10 17:44:40 -0400

    need to support more types in type cast

--- tests/code_gen_suite/expressions/type_cast3.go
+++ tests/code_gen_suite/expressions/type_cast3.go
@@ -2,14 +2,14 @@ package main
 
 // type length float64
 // type letter string
-type ap bool
+type ap rune
 
 func main () {
 	// var y = 4.5
 	// var y2 = "aejoif"
 	// var x length = length(y)
 	// var z letter = letter(y2)
-	var b ap = ap(true)
+	var b ap = ap('r')
 	// println(x)
 	// println(z)
 	println(b)
